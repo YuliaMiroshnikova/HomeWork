b45898b88a16683b24d458d7e0ea5262fabcffd8
Yu <mintprint202@gmail.com>
2024-02-24 23:24:47 +0200

    second commit

--- HomeWork5_git/Program.cs
+++ HomeWork5_git/Program.cs
@@ -2,3 +2,19 @@
 // git init
 // git remote add origin https://github.com/YuliaMiroshnikova/HomeWork.git
 // git add .
+// git commit -m "first commit"
+// git push -u origin main
+// git branch firstBranch
+// git checkout firstBranch
+// 
+// mkdir blog
+// touch blog/index.js blog/index.html
+
+// git add blog/index.js blog/index.html
+// git commit -m "add blog"
+
+// git checkout main
+// git add .
+// git commit -m «second commit"
+// git push -u origin main
+
